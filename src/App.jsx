ae03407311e2758c4989c5db794c5eb3f4eae8b1
Sean Schulte <sirsean@gmail.com>
2024-01-06 11:31:00 -0600

    convert ui

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { fetchBalance } from '@wagmi/core'
 
 import './App.css'
 import WETH_ABI from './assets/WETH.json';
+import W2PX_ABI from './assets/WethToPirex.json';
 
 // make sure we're pointed at the right address
 const W2PX_ADDRESS = '0x16DbF28Aa24678eCCe4dB7486b5061B2AF857FD0' // hardhat
@@ -54,29 +55,8 @@ function WalletConnector() {
   )
 }
 
-function BalanceManager() {
-  const { address } = useAccount();
-  const [wethBalance, setWethBalance] = useState(null);
-  const [pxethBalance, setPxethBalance] = useState(null);
-  const [apxethBalance, setApxethBalance] = useState(null);
-  useEffect(() => {
-    if (!address) {
-      return;
-    }
-    fetchBalance({
-      address: address,
-      token: WETH_ADDRESS,
-    }).then(setWethBalance)
-    fetchBalance({
-      address: address,
-      token: PXETH_ADDRESS,
-    }).then(setPxethBalance)
-    fetchBalance({
-      address: address,
-      token: APXETH_ADDRESS,
-    }).then(setApxethBalance)
-  }, [address])
-  if (address) {
+function BalanceManager({ wethBalance, pxethBalance, apxethBalance }) {
+  if (wethBalance || pxethBalance || apxethBalance) {
     return (
       <div className="BalanceManager">
         <table>
@@ -106,32 +86,8 @@ function BalanceManager() {
   }
 }
 
-function ApprovalManager() {
+function ApprovalManager({ wethBalance, wethApproval }) {
   const { address } = useAccount();
-  const [wethBalance, setWethBalance] = useState(null);
-  const [ wethApproval, setWethApproval ] = useState(null);
-  useEffect(() => {
-    if (!address) {
-      return;
-    }
-    fetchBalance({
-      address: address,
-      token: WETH_ADDRESS,
-    }).then(setWethBalance)
-  }, [address])
-  useContractRead({
-    address: WETH_ADDRESS,
-    abi: WETH_ABI,
-    functionName: 'allowance',
-    args: [address, W2PX_ADDRESS],
-    watch: true,
-    onSuccess(data) {
-      setWethApproval(data);
-    },
-    onError(error) {
-      console.log(error);
-    },
-  }, [address])
   const { data: tx, isLoading, isSuccess, write } = useContractWrite({
     address: WETH_ADDRESS,
     abi: WETH_ABI,
@@ -160,9 +116,103 @@ function ApprovalManager() {
   }
 }
 
+function Converter({ wethBalance, wethApproval }) {
+  const { address } = useAccount();
+  const { data: tx, isLoading, isSuccess, write } = useContractWrite({
+    address: W2PX_ADDRESS,
+    abi: W2PX_ABI,
+    functionName: 'convert',
+  })
+  const { data: receipt } = useWaitForTransaction({
+    hash: tx?.hash,
+    onSuccess(data) {
+      console.log(data);
+    },
+  })
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const receiver = formData.get('receiver');
+    const weth = formData.get('weth');
+    const shouldCompound = formData.get('shouldCompound') === 'true';
+    if (write) {
+      write({ args: [receiver, parseUnits(weth, 18), shouldCompound] });
+    }
+  }
+  if (!address || wethBalance == null || wethApproval == null || wethApproval < wethBalance.value) {
+    return null;
+  }
+  return (
+    <div className="Converter">
+      <h2>Convert your WETH to pxETH</h2>
+      {!isLoading && !isSuccess &&
+        <form onSubmit={onSubmit}>
+          <input type="text" name="weth" defaultValue={wethBalance?.formatted} />
+          <span>WETH -&gt;</span>
+          <select name="shouldCompound">
+            <option value="false">pxETH</option>
+            <option value="true">apxETH</option>
+          </select>
+          <button type="submit">Convert</button>
+          <br />
+          Receiver:
+          <input type="text" className="address" name="receiver" defaultValue={address} />
+        </form>}
+      {isLoading && <p>Signing...</p>}
+      {receipt &&
+        <>
+          <a target="_blank" href={`https://etherscan.io/tx/${receipt.transactionHash}`}>View on Etherscan</a>
+        </>}
+    </div>
+  )
+}
+
+function DataFetcher({ setWethBalance, setPxethBalance, setApxethBalance, setWethApproval }) {
+  const { address } = useAccount();
+  useEffect(() => {
+    if (!address) {
+      return;
+    }
+    fetchBalance({
+      address: address,
+      token: WETH_ADDRESS,
+    }).then(setWethBalance)
+    fetchBalance({
+      address: address,
+      token: PXETH_ADDRESS,
+    }).then(setPxethBalance)
+    fetchBalance({
+      address: address,
+      token: APXETH_ADDRESS,
+    }).then(setApxethBalance)
+  }, [address])
+  useContractRead({
+    address: WETH_ADDRESS,
+    abi: WETH_ABI,
+    functionName: 'allowance',
+    args: [address, W2PX_ADDRESS],
+    watch: true,
+    onSuccess: setWethApproval,
+    onError(error) {
+      console.log(error);
+    },
+  }, [address])
+  return null;
+}
+
 function HomePage() {
+  const [wethBalance, setWethBalance] = useState(null);
+  const [pxethBalance, setPxethBalance] = useState(null);
+  const [apxethBalance, setApxethBalance] = useState(null);
+  const [wethApproval, setWethApproval] = useState(null);
   return (
     <>
+      <DataFetcher
+        setWethBalance={setWethBalance}
+        setPxethBalance={setPxethBalance}
+        setApxethBalance={setApxethBalance}
+        setWethApproval={setWethApproval}
+        />
       <WalletConnector />
       <img src="/img/w2px.png" className="logo" alt="w2px logo" />
       <div className="content">
@@ -170,8 +220,19 @@ function HomePage() {
         <p>(WETH to pxETH ... or w2px for short. Get it?)</p>
         <p>And if you want to use it from a smart contract so that you can auto-convert WETH into pxETH to send along to its ultimate destination, well, that's also easy.</p>
         <p>This contract will take your WETH, withdraw it into ETH, and deposit that ETH into Pirex. It's up to you whether you want pxETH back, or autocompound into apxETH.</p>
-        <BalanceManager />
-        <ApprovalManager />
+        <BalanceManager
+          wethBalance={wethBalance}
+          pxethBalance={pxethBalance}
+          apxethBalance={apxethBalance}
+          />
+        <ApprovalManager
+          wethBalance={wethBalance}
+          wethApproval={wethApproval}
+          />
+        <Converter
+          wethBalance={wethBalance}
+          wethApproval={wethApproval}
+          />
         <hr />
         <ul>
           // TODO: fill out these links
